6fa41f98c29e58c58f53ca4260a4c5eb17196434
Alex McHale <alex@anticlever.com>
2013-09-28 09:07:29 -0500

    Fix the endpoint used for team standings.

--- lib/xmlstats/endpoints/nba_team_results.rb
+++ lib/xmlstats/endpoints/nba_team_results.rb
@@ -5,7 +5,7 @@ class Xmlstats::Endpoints::NbaTeamResults
   def self.fetch(team_id = nil, season = nil)
     raise "please specify team_id as detailed in https://erikberg.com/api#teamresults" unless team_id
     params = { season: season }
-    response = fetch_json("nba/team/#{team_id}/results", params)
+    response = fetch_json("nba/results/#{team_id}", params)
     [ response ].flatten.map do |result|
       Xmlstats::Objects::TeamResult.new result
     end
