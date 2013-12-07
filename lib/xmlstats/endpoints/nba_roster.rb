3dd87493937f9b0e659418ab0209e8c8ad9dc451
oren <oren@oren-MS-7367.(none)>
2013-12-07 10:59:43 +0200

    changing nba roster api path

--- lib/xmlstats/endpoints/nba_roster.rb
+++ lib/xmlstats/endpoints/nba_roster.rb
@@ -4,7 +4,7 @@ class Xmlstats::Endpoints::NbaRoster
 
   def self.fetch(team_id = nil)
     raise "please specify team_id as detailed in https://erikberg.com/api/issues/135" unless team_id
-    response = fetch_json("nba/team/#{team_id}/roster")
+    response = fetch_json("nba/roster/#{team_id}")
     [ response ].flatten.map do |result|
       Xmlstats::Objects::Player.new result["player"]
     end
