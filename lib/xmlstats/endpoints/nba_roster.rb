b3d5cd1330be5ea76cd0b873169cdf620d151191
oren <oren@oren-MS-7367.(none)>
2013-12-08 20:51:33 +0200

    another fix to nba roster api

--- lib/xmlstats/endpoints/nba_roster.rb
+++ lib/xmlstats/endpoints/nba_roster.rb
@@ -5,8 +5,8 @@ class Xmlstats::Endpoints::NbaRoster
   def self.fetch(team_id = nil)
     raise "please specify team_id as detailed in https://erikberg.com/api/issues/135" unless team_id
     response = fetch_json("nba/roster/#{team_id}")
-    [ response ].flatten.map do |result|
-      Xmlstats::Objects::Player.new result["player"]
+    response["players"].map do |player_hash|
+      Xmlstats::Objects::Player.new player_hash
     end
   end
 
