b7600773c9de8268fe6317e3184fe178fee9f1aa
Alex McHale <alex@anticlever.com>
2015-04-12 06:54:28 -0500

    style tweaks

--- lib/xmlstats/endpoints/mlb_roster.rb
+++ lib/xmlstats/endpoints/mlb_roster.rb
@@ -3,12 +3,11 @@ class Xmlstats::Endpoints::MlbRoster
   include Xmlstats::Endpoint
 
   def self.fetch(team_id = nil, expanded = false)
-    raise "please specify team_id as detailed in https://erikberg.com/api/issues/135" unless team_id
+    fail "please specify team_id as detailed in https://erikberg.com/api/issues/135" unless team_id
 
     params = {}
-    if expanded
-    	params = {status: "expanded"} #40-men roster please see https://erikberg.com/api/methods/roster 
-    end
+
+    params = { status: "expanded" } if expanded
 
     response = fetch_json("mlb/roster/#{team_id}", params)
 
